8bbc88066aba854b2b5ab8aec92ebcca716f5780
Olivier <coutanto1@eduroam-107087.grenet.fr>
2023-12-06 13:11:29 +0100

    maj

--- MOMENT_DISP_F90_OPENMP/EXAMPLE/example.py
+++ MOMENT_DISP_F90_OPENMP/EXAMPLE/example.py
@@ -3,9 +3,8 @@
 
 # In[1]:
 
-
-get_ipython().run_line_magic('load_ext', 'autoreload')
-get_ipython().run_line_magic('autoreload', '2')
+import sys
+sys.path.append(\"../src\")
 import numpy as np
 from axitra import *
 import matplotlib.pyplot as pt
